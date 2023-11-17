fafdd48b45a483bc708daffbecae5a3c5c000c7a
Souza1999 <bryurisouza@outlook.com.br>
2023-11-17 14:52:21 -0300

    criação de PEDIDOS.ts

---
+++ src/app/shared/model/PEDIDOS.ts
@@ -0,0 +1,12 @@
+export const PEDIDOS = [
+    {
+        id: 1,
+        nome: 'Adriana',
+        descricao: '100g de maminha (mal)',
+    },
+    {
+        id: 2,
+        nome: 'Beatriz',
+        descricao: '200g de picanha (mal)',
+    },
+];
