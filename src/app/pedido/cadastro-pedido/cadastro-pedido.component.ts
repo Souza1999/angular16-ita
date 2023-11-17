06d5e521c81895afdc555fc489e7b726c64e8825
Souza1999 <bryurisouza@outlook.com.br>
2023-11-17 13:10:51 -0300

    Colocando o implements OnInit

--- src/app/pedido/cadastro-pedido/cadastro-pedido.component.ts
+++ src/app/pedido/cadastro-pedido/cadastro-pedido.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-pedido',
   templateUrl: './cadastro-pedido.component.html',
   styleUrls: ['./cadastro-pedido.component.scss']
 })
-export class CadastroPedidoComponent {
+export class CadastroPedidoComponent implements OnInit {
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
