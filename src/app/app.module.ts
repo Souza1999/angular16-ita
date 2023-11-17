7feafe1b50916516530af191cfa5b84eec5210e6
Souza1999 <bryurisouza@outlook.com.br>
2023-11-17 10:40:15 -0300

    coloquei o MatFormField e importei corretamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule // Tive que colocar o formsmodule para que o ngModel lá no html funcionasse
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
