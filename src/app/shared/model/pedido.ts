86c76f70adf3b550c00d68459e02796b7d1933ca
Souza1999 <bryurisouza@outlook.com.br>
2023-12-08 19:18:51 -0300

    Colocando um comentário e testando o commit

--- src/app/shared/model/pedido.ts
+++ src/app/shared/model/pedido.ts
@@ -3,3 +3,5 @@ export class Pedido {
     nome: string = '';
     descricao: string = '';
 }
+
+// Como faço para poder inicializar as variáveis sem atribuir valor? Para que na interface web, não apareça o "0" no id, mas que fique um campo limpo.
