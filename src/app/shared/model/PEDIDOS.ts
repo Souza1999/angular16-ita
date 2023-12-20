0fa2a1dc36d6d65f5336d05c7f31377a96beca50
Yuri Souza <bryurisouza@outlook.com.br>
2023-12-20 19:34:32 -0300

    acrescentando um pedido

--- src/app/shared/model/PEDIDOS.ts
+++ src/app/shared/model/PEDIDOS.ts
@@ -14,4 +14,9 @@ export const PEDIDOS = [
         nome: 'Carlos',
         descricao: '3 porções de salpicão',
     },
+    {
+        id: 4,
+        nome: 'Daniel',
+        descricao: '400g de cupim, 1kg de picanha (ao ponto e com pouca gordura), 2 porções de batata frita, 2 porções de salpicão e 4 latas de coca cola zero.',
+    },
 ];
