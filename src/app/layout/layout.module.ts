546c4d7cea311baeee4115edeb2657470c89ebbf
Yuri Souza <bryurisouza@outlook.com.br>
2023-12-20 14:30:13 -0300

    importando e exportando no layout.module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 
@@ -8,8 +11,15 @@ import { MenuComponent } from './menu/menu.component';
   declarations: [
     MenuComponent
   ],
+  exports: [
+    MenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    // O MatMenuModule não tava sendo importado, aí tive que importar primeiro lá em cima e depois colocar aqui no imports.
   ]
 })
 export class LayoutModule { }
