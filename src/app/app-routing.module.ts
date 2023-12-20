77244aed5119495813a68afd99813216b038a810
Yuri Souza <bryurisouza@outlook.com.br>
2023-12-20 13:11:46 -0300

    Concluindo as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: CadastroPedidoComponent
   },
   {
-    path: 'lista-pedido/:id',
+    path: 'listagem-pedido',
     component: ListagemPedidoComponent
   },
 
