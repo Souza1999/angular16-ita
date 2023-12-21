5e03dc148984b53e71338c7bedc53d2dc0a7ccbc
Yuri Souza <bryurisouza@outlook.com.br>
2023-12-21 02:06:58 -0300

    commit vazio que a IDE tá sugerindo

--- src/app/pedido/listagem-pedido/listagem-pedido.component.ts
+++ src/app/pedido/listagem-pedido/listagem-pedido.component.ts
@@ -10,7 +10,7 @@ import { PedidoService } from 'src/app/shared/services/pedido.service';
 export class ListagemPedidoComponent implements OnInit {
 
   pedidos: Array<Pedido> = [];  // É o seguinte, o professor deixou passar esse erro, mas pesquisei e vi que basta colocar esses colchetes. Então ficar atento se isso pode dar problema.
-
+  
   titulo = "Listagem de Pedidos";
   
   constructor(private pedidoService: PedidoService) {
